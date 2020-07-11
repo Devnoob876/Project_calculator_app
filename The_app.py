8924596c06509115a2232c40c107df25d7f4db0a
NOOB <fool0316@gmail.com>
2020-07-11 11:48:41 +0600

    Update The_app.py

--- The_app.py
+++ The_app.py
@@ -3,41 +3,15 @@ import tkinter.messagebox
 import os
 import datetime
 import wikipedia
-def exit_app():
-	exit()
-
-def back_farme_1():
-	frame.pack()
-
-def delete_frame():
-	frame.pack_forget()
+def new_windows():
 	
-	frm1.pack()
-
-
-
-
-
-	lbl1 = tk.Label(frm1, fg= "red",text="this is second frame if you click the button it will inform new frame", font=("Bold", 22))
-	lbl1.pack()
-	exbtn = tk.Button(frm1, fg="red", text="exit programm", font=("Italic"), command=exit_app)
-	exbtn.pack()
-	btn_back = tk.Button(frm1, fg="black", command=back_farme_1, font=("Roboto"), text="lol")
-	btn_back.pack()
-
-
 
 root = tk.Tk()
-root.title("Learn pc")
 
-frame = tk.Frame(root, width="2000", height= '2000')
-frame.pack()
-label = tk.Label(frame, fg= 'black',text="Learn your computer", font=("Italic", 22))
+label = tk.Label(text="learn pc")
 label.pack()
 
-frm1 = tk.Frame(root,width="2000", height= '2000')
-btn1 = tk.Button(frame, fg='red', font=("bold", 11), command=delete_frame, text="delete the text frame")
-btn1.pack()
+
 
 
 root.geometry('2000x2000')
@@ -55,7 +29,7 @@ root.mainloop()
 ##############################
 #################################
 ########################################
-#########################################
+##############################################
 ####################################
 ###############################
 #######################
